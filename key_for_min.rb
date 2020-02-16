7dc2b7029a20dcf9828d3535ca6a7fdfe909fecf
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-16 06:43:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,12 @@
 
 def key_for_min_value(name_hash)
   smallest_value = 1/0.0
+  smallest_key = ""
   name_hash.each do |key, val|
-    if val <
+    if val < smallest_value
+      smallest_value = val
+      smallest_key = key
+    end
   end
+  name_hash[smallest_key]
 end
